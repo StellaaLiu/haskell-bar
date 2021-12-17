34f3cc97bbe6adc3976c0d3fe1f5fa9c93ccfc9c
Stella Liu <91271675+StellaaLiu@users.noreply.github.com>
2021-12-17 15:52:51 +0000

    Update Parse.hs

--- groupwork/src/Parse.hs
+++ groupwork/src/Parse.hs
@@ -11,3 +11,66 @@ import Types
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as L8
 
+instance FromJSON Overall where
+    parseJSON (Object v) =
+        Overall <$> v .: "tank_name"
+                <*> v .: "nation"
+                <*> v .: "tank_type"
+                <*> v .: "tier"
+                <*> v .: "players"
+                <*> v .: "average_battles"
+                <*> v .: "average_damage"
+                <*> v .: "average_win_rate"
+                <*> v .: "average_wn8"
+    parseJSON _ = mzero
+
+instance ToJSON Overall where
+    toJSON (Overall tank_name nation tank_type tier players average_battles average_damage average_win_rate average_wn8) =
+        object [ "tank_name" .= tank_name
+               , "nation" .= nation
+               , "tank_type" .= tank_type
+               , "tier" .= tier
+               , "players" .= players
+               , "average_battles" .= average_battles
+               , "average_damage" .= average_damage
+               , "average_win_rate" .= average_win_rate
+               , "average_wn8" .= average_wn8
+               ]
+
+instance FromJSON SpecByNation where
+    parseJSON (Object v) =
+        SpecByNation <$> v .: "nation"
+                     <*> v .: "tier"
+                     <*> v .: "players"
+                     <*> v .: "average_win_rate"
+                     <*> v .: "average_wn8"
+    parseJSON _ = mzero
+
+instance ToJSON SpecByNation where
+    toJSON (SpecByNation nation_ tier_ players_ average_win_rate_ average_wn8_) =
+        object [ "nation" .= nation_
+               , "tier" .= tier_
+               , "players" .= players_
+               , "average_win_rate" .= average_win_rate_
+               , "average_wn8" .= average_wn8_
+               ]
+
+instance FromJSON SpecByType where
+    parseJSON (Object v) =
+        SpecByType <$> v .: "tank_type"
+                   <*> v .: "tier"
+                   <*> v .: "players"
+                   <*> v .: "average_win_rate"
+                   <*> v .: "average_wn8"
+    parseJSON _ = mzero
+
+instance ToJSON SpecByType where
+    toJSON (SpecByType tank_type_ tier__ players__ average_win_rate__ average_wn8__) =
+        object [ "tank_type" .= tank_type_
+               , "tier" .= tier__
+               , "players" .= players__
+               , "average_win_rate" .= average_win_rate__
+               , "average_wn8" .= average_wn8__
+               ]
+
+parseRecords :: L8.ByteString -> Either String Records
